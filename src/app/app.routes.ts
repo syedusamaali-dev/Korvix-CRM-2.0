016e1a9d94196004e36c0460f885e2ea563694b2
Usama2201 <usamaali191@gmail.com>
2024-01-02 17:30:00 +0500

    Clean Process - Korvix-2.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,51 +17,51 @@ export const routes: Routes = [
         path: 'dashboard',
         loadChildren: () => import('./views/dashboard/routes').then((m) => m.routes)
       },
-      {
-        path: 'theme',
-        loadChildren: () => import('./views/theme/routes').then((m) => m.routes)
-      },
-      {
-        path: 'base',
-        loadChildren: () => import('./views/base/routes').then((m) => m.routes)
-      },
-      {
-        path: 'buttons',
-        loadChildren: () => import('./views/buttons/routes').then((m) => m.routes)
-      },
-      {
-        path: 'forms',
-        loadChildren: () => import('./views/forms/routes').then((m) => m.routes)
-      },
-      {
-        path: 'icons',
-        loadChildren: () => import('./views/icons/routes').then((m) => m.routes)
-      },
-      {
-        path: 'notifications',
-        loadChildren: () => import('./views/notifications/routes').then((m) => m.routes)
-      },
-      {
-        path: 'widgets',
-        loadChildren: () => import('./views/widgets/routes').then((m) => m.routes)
-      },
-      {
-        path: 'charts',
-        loadChildren: () => import('./views/charts/routes').then((m) => m.routes)
-      },
-      {
-        path: 'pages',
-        loadChildren: () => import('./views/pages/routes').then((m) => m.routes)
-      }
+      // {
+      //   path: 'theme',
+      //   loadChildren: () => import('./views/theme/routes').then((m) => m.routes)
+      // },
+      // {
+      //   path: 'base',
+      //   loadChildren: () => import('./views/base/routes').then((m) => m.routes)
+      // },
+      // {
+      //   path: 'buttons',
+      //   loadChildren: () => import('./views/buttons/routes').then((m) => m.routes)
+      // },
+      // {
+      //   path: 'forms',
+      //   loadChildren: () => import('./views/forms/routes').then((m) => m.routes)
+      // },
+      // {
+      //   path: 'icons',
+      //   loadChildren: () => import('./views/icons/routes').then((m) => m.routes)
+      // },
+      // {
+      //   path: 'notifications',
+      //   loadChildren: () => import('./views/notifications/routes').then((m) => m.routes)
+      // },
+      // {
+      //   path: 'widgets',
+      //   loadChildren: () => import('./views/widgets/routes').then((m) => m.routes)
+      // },
+      // {
+      //   path: 'charts',
+      //   loadChildren: () => import('./views/charts/routes').then((m) => m.routes)
+      // },
+      // {
+      //   path: 'pages',
+      //   loadChildren: () => import('./views/pages/routes').then((m) => m.routes)
+      // }
     ]
   },
-  {
-    path: '404',
-    loadComponent: () => import('./views/pages/page404/page404.component').then(m => m.Page404Component),
-    data: {
-      title: 'Page 404'
-    }
-  },
+  // {
+  //   path: '404',
+  //   loadComponent: () => import('./views/pages/page404/page404.component').then(m => m.Page404Component),
+  //   data: {
+  //     title: 'Page 404'
+  //   }
+  // },
   {
     path: '500',
     loadComponent: () => import('./views/pages/page500/page500.component').then(m => m.Page500Component),
