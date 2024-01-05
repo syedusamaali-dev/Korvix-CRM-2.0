e854499b3b0f1c8a9557cd0314cc6ea8fb900d0e
Usama2201 <usamaali191@gmail.com>
2024-01-05 10:30:00 +0500

    Complie Error Fixed - Korvix-2.0

--- src/components/docs-callout/docs-callout.component.ts
+++ src/components/docs-callout/docs-callout.component.ts
@@ -18,10 +18,10 @@ export class DocsCalloutComponent {
 
   readonly href = computed(() => {
     const version = packageJson?.config?.coreui_library_short_version;
-    const docsUrl = packageJson?.config?.coreui_library_docs_url ?? 'https://coreui.io/angular/';
+    // const docsUrl = packageJson?.config?.coreui_library_docs_url ?? 'https://coreui.io/angular/';
     const href = this.hrefInput();
     // const path: string = version ? `${version}/${href}` : `${href}`;
     const path: string = href;
-    return `${docsUrl}${path}`;
+    // return `${docsUrl}${path}`;
   });
 }
--- src/components/docs-example/docs-example.component.ts
+++ src/components/docs-example/docs-example.component.ts
@@ -23,11 +23,11 @@ export class DocsExampleComponent {
 
   readonly href = computed(() => {
     const version = packageJson?.config?.coreui_library_short_version;
-    const docsUrl = packageJson?.config?.coreui_library_docs_url ?? 'https://coreui.io/angular/';
+    // const docsUrl = packageJson?.config?.coreui_library_docs_url ?? 'https://coreui.io/angular/';
     const href = this.hrefInput();
     // const path: string = version ? `${version}/${href}` : `${href}`;
     const path: string = href;
     this.#changeDetectorRef.markForCheck();
-    return `${docsUrl}${path}`;
+    // return `${docsUrl}${path}`;
   });
 }
