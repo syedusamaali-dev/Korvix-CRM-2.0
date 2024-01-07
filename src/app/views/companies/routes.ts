6cbaf24a59398541936251ba249dc78ad4c9b884
Usama2201 <usamaali191@gmail.com>
2024-01-07 10:30:00 +0500

    Contact Route Added update - Korvix-2.0

--- src/app/views/companies/routes.ts
+++ src/app/views/companies/routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 
-declare const $localize: any;
-
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
       import('./companies.component').then((m) => m.CompaniesComponent),
-    data: {
-      title: $localize`Companies`,
-    },
   },
 ];
