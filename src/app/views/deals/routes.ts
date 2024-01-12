15713b22b862c9cdd1bcf98a6132ed7fb9985530
Usama2201 <usamaali191@gmail.com>
2024-01-12 10:30:00 +0500

    Added Deal Route Fix - Korvix-2.0

--- src/app/views/deals/routes.ts
+++ src/app/views/deals/routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 
-declare const $localize: any;
-
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
       import('./deals.component').then((m) => m.DealsComponent),
-    data: {
-      title: $localize`Dashboard`,
-    },
   },
 ];
